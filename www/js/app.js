cece95fe20bf22a420c120c4cbf5d1ba60eb77df
jammiM <jammim19@gmail.com>
2018-02-11 18:17:09 +0000

    Adding a custom green colour for buttons

--- www/js/app.js
+++ www/js/app.js
@@ -26,13 +26,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
   $ionicConfigProvider.platform.android.tabs.position('bottom');
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
 
-  // setup an abstract state for the tabs directive
+  $stateProvider
     .state('tab', {
     url: '/tab',
     abstract: true,
